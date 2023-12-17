74af684d28973286ff4cb5908588d19ee0d86b1e
Gunnkaruntancharoen <98071227+Ejokr@users.noreply.github.com>
2023-12-17 17:52:34 +0700

    I finished the call introduction screen.

--- components/introduction/CallIntroductionScreen.jsx
+++ components/introduction/CallIntroductionScreen.jsx
@@ -1,6 +1,20 @@
-import { View, Text, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import Styles from "./CallIntroductionScreenStyle";
 const CallIntroductionScreen = () => {
+    const [ElapsedTime, SetElapsedTime] = useState(0)
+    useEffect(() => {
+        const Timer = setInterval(() => {
+            SetElapsedTime(PreviousTime => PreviousTime + 1)
+        }, 1000)
+        return () => clearInterval(Timer)
+    }, [])
+    const FormatTime = (Seconds) => {
+        const Minutes = Math.floor(Seconds / 60)
+        const RemainingSeconds = Seconds % 60
+        return `${String(Minutes).padStart(2, '0')}:${String(RemainingSeconds).padStart(2, '0')}`
+    }
     return (
         <View
             style={Styles.Container}
@@ -21,6 +35,58 @@ const CallIntroductionScreen = () => {
                 <Text
                     style={Styles.ScreenTitle}
                 >wake-up call</Text>
+            </View>
+            <Image
+                source={require('../../assets/images/Waki.gif')}
+                style={Styles.WakiImage}
+            />
+            <Text
+                style={Styles.InCallWithText}
+            >In call with</Text>
+            <Text
+                style={Styles.RoleText}
+            >Grandmother</Text>
+            <Text
+                style={Styles.CallDuration}
+            >{FormatTime(ElapsedTime)}</Text>
+            <View
+                style={Styles.CallMenuContainer}
+            >
+                <View
+                    style={Styles.WhiteIconContainers}
+                >
+                    <Ionicons
+                        name="volume-medium-outline"
+                        size={36}
+                    />
+                </View>
+                <TouchableOpacity
+                    style={Styles.WhiteIconContainers}
+                >
+                    <Ionicons
+                        name="pause-outline"
+                        size={36}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={Styles.RedIconContainer}
+                >
+                    <Ionicons
+                        name="close-outline"
+                        size={36}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View
+                style={Styles.NextButtonContainer}
+            >
+                <TouchableOpacity
+                    style={Styles.NextButton}
+                >
+                    <Text
+                        style={Styles.NextText}
+                    >Next</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
--- components/introduction/CallIntroductionScreenStyle.js
+++ components/introduction/CallIntroductionScreenStyle.js
@@ -32,6 +32,77 @@ const Styles = StyleSheet.create({
             width: 1
         },
         textShadowRadius: 4
+    },
+    WakiImage: {
+        height: 208,
+        width: 243
+    },
+    InCallWithText: {
+        fontFamily: Fonts.Medium,
+        fontSize: 20
+    },
+    RoleText: {
+        fontFamily: Fonts.Bold,
+        fontSize: 30,
+        marginTop: 8
+    },
+    CallDuration: {
+        fontFamily: Fonts.Medium,
+        fontSize: 20,
+        marginTop: 8
+    },
+    CallMenuContainer: {
+        flexDirection: 'row',
+        marginTop: 43
+    },
+    WhiteIconContainers: {
+        alignItems: 'center',
+        backgroundColor: Colors.WhiteIconContainer,
+        borderRadius: 100,
+        height: 80,
+        justifyContent: 'center',
+        marginHorizontal: 14,
+        opacity: 0.85,
+        width: 80
+    },
+    RedIconContainer: {
+        alignItems: 'center',
+        backgroundColor: Colors.RedIconContainer,
+        borderRadius: 100,
+        height: 80,
+        justifyContent: 'center',
+        marginHorizontal: 14,
+        opacity: 0.8,
+        width: 80
+    },
+    NextButtonContainer: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    NextButton: {
+        alignItems: 'center',
+        backgroundColor: Colors.Yellow,
+        borderRadius: 25,
+        elevation: 4,
+        height: 55,
+        justifyContent: 'center',
+        marginBottom: 100,
+        width: 150,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'rgba(0, 0, 0)',
+                shadowOffset: {
+                    height: 2,
+                    width: 1
+                },
+                shadowRadius: 4,
+                shadowOpacity: 0.25
+            }
+        })
+    },
+    NextText: {
+        fontFamily: Fonts.SemiBold,
+        fontSize: 20
     }
 })
 export default Styles
